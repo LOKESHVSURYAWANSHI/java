3c36d32191e0a4a83a4cdb68a4be70daee435e07
lokesh <lokeshsuryawanshi35@gmail.com>
2026-03-28 11:39:54 +0530

    first commit

---
+++ src/first/increment.java
@@ -0,0 +1,33 @@
+package first;
+
+public class increment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int num=10;
+//		System.out.println(num++ + num + ++num);
+//		System.out.println(num);
+		
+//		int a=30;
+//		System.out.println(a++ + ++a - a--);
+//		System.out.println(a);
+		
+//		int num=20;
+//		System.out.println(++num + ++num - ++num);
+//		System.out.println(num);
+		
+//		int num=5;
+//		System.out.println((num++) + (++num) + (++num) + (num++) + num+35);
+//		System.out.println(num);
+		
+//		int num =6;
+//		System.out.println(num++ + ++num - num++ - --num + num++);
+//		System.out.println(num);
+		
+		int a=35;
+		System.out.println();
+		                 
+	}
+	
+
+}
