ffc36896ea37616511e135334c6a5bb851b6d9ef
lokesh <lokeshsuryawanshi35@gmail.com>
2026-03-28 16:23:38 +0530

    increment practice

--- src/first/increment.java
+++ src/first/increment.java
@@ -24,9 +24,134 @@ public class increment {
 //		System.out.println(num++ + ++num - num++ - --num + num++);
 //		System.out.println(num);
 		
-		int a=35;
-		System.out.println();
-		                 
+//		int a=35;
+//		System.out.println(a-- + a++ - ++a + ++a + ++a);
+//		System.out.println(a);
+		
+//		int a = 10;
+//		System.out.println(a++ + a++);
+//		            a=11  a=10 / a=12  a=11     
+//		System.out.println(a);
+		
+//		int a = 5;
+//		System.out.println(++a + ++a);
+//		                   a=6  + a=7
+//		System.out.println(a);
+		
+//		int a = 7;
+//		System.out.println(a-- + --a);
+//		              a=6 a=7/  a=5
+//		System.out.println(a);
+		
+		
+//		int a = 8;
+//		System.out.println(a++ + ++a + a);
+//	                  a=9 a=8  / a=10/10
+//		System.out.println(a);
+		
+//		int a = 12;
+//		System.out.println(--a + a++ + ++a);
+//					      a=11 / a=12 a=11/a=13
+//		System.out.println(a);
+		
+//		int a = 6;
+//		System.out.println(a++ + a++ + ++a);
+//                 a=9     6   +7    +  9
+//		System.out.println(a);
+		
+//		int a = 15;
+//		System.out.println(a++ - ++a + a--);
+//                     a=16  a=15-17 + a=16  a=17
+//		System.out.println(a);
+		
+//		int a = 20;
+//		System.out.println(++a + a-- - ++a + a++);
+//		                a=21 + a=21/a=20 - a=21 + a=21/a=22
+//		System.out.println(a);
+		
+//		int a = 9;
+//		System.out.println(a-- + ++a + a++ - --a);
+//		                (a=8)a=9+a=9+(a=10)a=9-a=9
+//		System.out.println(a);
+		
+//		int a = 25;
+//		System.out.println(a++ + ++a + a++ + ++a);
+//		            (a=26)a=25+a=27+(a=28)a=27+a=29
+//		System.out.println(a);
+		
+//		int a = 30;
+//		System.out.println(a-- - --a + a++ + ++a);
+//		             (a=29)a=30-a=28+(a=29)a=28+a=30
+//		System.out.println(a);
+		
+		
+//		int a = 18;
+//		System.out.println(++a + ++a - a++ + a--);
+//		               a=19+a=20-(a=21)a=20+(a=20)a=21
+//		System.out.println(a);
+		
+//		int a = 5;
+//		int b = a++ + ++a + a++ + ++a;
+//		  (a=6)a=5+a=7+(a=8)a=7+a=9
+//		System.out.println(b);
+//		System.out.println(a);
+		
+//		int a = 10;
+//		int b = a-- + --a + a++ + ++a + a;
+//		      a=10 + a=8 + a=8+a=10 +10
+//		System.out.println(b);
+//		System.out.println(a);
+		
+//		int a = 3;
+//		System.out.println(a++ + a++ + ++a + a-- + --a + a);
+//		                   a=3+a=4+a=6+a=6+a=4+a=4
+//		System.out.println(a);
+		
+		
+//		int a = 5, b = 10;
+//		int c = a++ + ++b + a++ + b++ + ++a;
+//		(a=8)(b=12) a=5+b=11+a=6+b=11+a=8
+//		System.out.println(c);
+//		System.out.println(a + " " + b);
+		
+//		int a = 3, b = 4;
+//		int c = ++a + b++ + ++b + a++ + b + a;
+//		(a=5)(b=6) a=4+b=4+b=6+a=4+a=5+b=6 
+//		System.out.println(c);
+//		System.out.println(a + " " + b);
+		
+//		int a = 7, b = 2;
+//		int c = a++ * ++b + ++a * b++ + a;
+//		(a=9)(b=4) a=7*b=3+a=9*b=3+a=9
+//		System.out.println(c);
+//		System.out.println(a + " " + b);
+		
+		
+//		int a = 5;
+//		int b = (a++) + (++a) + (a = a + 5) + (a++);
+//		        5     +   7   +      12     +  12
+//		System.out.println(b);
+//		System.out.println(a);
+		
+//		int a = 10;
+//		int b = a++ + (a = 20) + ++a + a++;
+//		        10  + 20       +  21 + 21
+//		System.out.println(b);
+//		System.out.println(a);
+		
+//		int a = 4;
+//		int b = ++a + (a++ + (a = 10)) + ++a;
+//		          5 +  5   +10         +  11
+//		System.out.println(b);
+//		System.out.println(a);
+		
+//		int a = 2;
+//		int b = a++ + (a++ + (++a + a++)) + ++a;
+//		        2   +  3   +   5  +  5    + 7
+//		System.out.println(b);
+//		System.out.println(a);
+		
+		
 	}
 	
 
